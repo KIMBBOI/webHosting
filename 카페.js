5c8dd0f75ed565813a586287ee36d0b1fc71247b
KIMBBOI <142900456+KIMBBOI@users.noreply.github.com>
2023-11-07 18:20:15 +0900

    Add files via upload

---
+++ 카페.js
@@ -0,0 +1,23 @@
+
+// const inputTag = document.querySelector("#search");
+// const result = document.querySelector("#search");
+// inputTag.addEventListener("mouseover", () => {
+//     inputTag.innerHTML = "내용을 입력하세요.";
+// });
+// const inputTag2 = document.getElementById("search");
+// inputTag2.style.fontSize = '10px';
+
+
+function f01(inputElement) {
+    inputElement.placeholder = "내용을 입력하세요.";
+  }
+  
+  function hidePlaceholder(inputElement) {
+    inputElement.placeholder = "";
+  }
+
+
+// function f01(){
+//     const out = document.querySelector("#search");
+//     out.innerHTML = "내용을 입력하세요.";
+// }
